b7fe6cc667c71b4436b7a0c5036b99c51bab2562
Abdallah Hocine <hocineabdallah11@gmail.com>
2024-12-03 16:51:40 +0100

    Phase 24 : déploiement

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -44,7 +44,7 @@ class ContactController extends AbstractController {
     public function sendEmail(MailerInterface $mailer, Contact $contact){
         $email = (new Email())
             ->from($contact->getEmail())
-            ->to('contact@mesvoyages.com')
+            ->to('abdz139375@gmail.com')
             ->subject('Message du site de voyages')
             ->html($this->renderView(
                     'pages/_email.html.twig', [
